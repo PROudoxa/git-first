35c6dd8ea3af27237fe45cfb78155bdd1e5cff6b
PROudoxa <my.favourite.job777@gmail.com>
2016-05-09 15:57:52 +0300

    created file for tips from book "Vasiliy Usov, Swift_Osnovy_razrabotki..."

---
+++ playground/lessons_Geekbrains.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var `var` = "cheat" // если имя переменной совпадает с именами служебных операторов, то надо `...`
+`var`
+
+
+
+
