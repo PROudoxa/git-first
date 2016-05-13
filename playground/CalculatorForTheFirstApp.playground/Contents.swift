7411fe2f4365b48aba553f5f2dc6568d856167a7
PROudoxa <my.favourite.job777@gmail.com>
2016-05-13 11:23:54 +0300

    created new project to get to known with TABLEVIEW in the future

---
+++ playground/CalculatorForTheFirstApp.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+let tutorialTeam: Int = 60
+let editorialTeam: Int = 17
+let totalTeam: Int = tutorialTeam + editorialTeam
+
+
