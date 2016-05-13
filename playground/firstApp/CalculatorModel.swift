3fae7c08b7235eac2dae5fc01f78e6699b71b920
PROudoxa <my.favourite.job777@gmail.com>
2016-05-13 13:50:05 +0300

    created calculator. its first my app

---
+++ playground/firstApp/CalculatorModel.swift
@@ -0,0 +1,43 @@
+//
+//  CalculatorModel.swift
+//  Calculator
+//
+//  Created by Алексей on 13.05.16.
+//  Copyright © 2016 Алексей. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorModel {
+    
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    func returnPossibleTips() -> [Int: Double] {
+        
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+        
+    }
+}
+
