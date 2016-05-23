bf3517376264418cd34c3b96f0b96965faa4a56d
Alex Voronov <my.favourite.job777@gmail.com>
2016-05-23 12:46:24 +0300

    this commit is before fixing the problem: link delay. the right link is showing as the next one.

--- playground/Sities&Countries/MoreInformation.swift
+++ playground/Sities&Countries/MoreInformation.swift
@@ -8,88 +8,50 @@
 
 import UIKit
 
-class MoreInformation: UITableViewController {
+class MoreInformation: UIViewController {
+    
+    var country: String = ""
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    // dictionary of links for countries
+    let countriesWikiURL: [String: String] = [
+        "Ukraine":  "https://ru.wikipedia.org/wiki/%D0%A3%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0",
+        "USA":      "https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%B5%D0%B4%D0%B8%D0%BD%D1%91%D0%BD%D0%BD%D1%8B%D0%B5_%D0%A8%D1%82%D0%B0%D1%82%D1%8B_%D0%90%D0%BC%D0%B5%D1%80%D0%B8%D0%BA%D0%B8",
+        "United Kingdom":       "https://ru.wikipedia.org/wiki/%D0%92%D0%B5%D0%BB%D0%B8%D0%BA%D0%BE%D0%B1%D1%80%D0%B8%D1%82%D0%B0%D0%BD%D0%B8%D1%8F",
+        "Canada":   "https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D0%BD%D0%B0%D0%B4%D0%B0"
+    ]
+    
+    var url: String = "https://ru.wikipedia.org"
+    
+    func  makeUrl (country: String) -> String {
+        if country == "Ukraine" {
+            url = countriesWikiURL["Ukraine"]!
+        } else if country == "USA" {
+            url = countriesWikiURL["USA"]!
+        } else if country == "United Kingdom" {
+            url = countriesWikiURL["United Kingdom"]!
+        } else if country == "Canada" {
+            url = countriesWikiURL["Canada"]!
+        }
+        return url
+    }
+   
+    func makeUrlSwitch(country: String) -> String {
+        switch country {
+        case "Ukraine":        url = countriesWikiURL["Ukraine"]!
+        case "USA":            url = countriesWikiURL["USA"]!
+        case "United Kingdom": url = countriesWikiURL["United Kingdom"]!
+        case "Canada":         url = countriesWikiURL["Canada"]!
+        default:               url = "https://ru.wikipedia.org"
+        }
+        return url
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        url = makeUrl(country)
+        let request = NSURLRequest(URL: NSURL(string:url)!)
+        webView.loadRequest(request)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
