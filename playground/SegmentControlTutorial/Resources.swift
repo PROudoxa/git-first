1e1a9b417c4a9e4a6556e9d73853915122753022
Alex Voronov <my.favourite.job777@gmail.com>
2016-05-26 02:04:11 +0300

    testing project. is not need now.

---
+++ playground/SegmentControlTutorial/Resources.swift
@@ -0,0 +1,29 @@
+//
+//  Resources.swift
+//  SegmentControlTutorial
+//
+//  Created by Алексей on 25.05.16.
+//  Copyright © 2016 Alex&V. All rights reserved.
+//
+
+import Foundation
+
+
+class Resources: NSObject {
+    
+    //Array for Europe countries
+    let europeCountries: [String] = [
+        "Ger",
+        "Bel",
+        "Fra"
+    ]
+    
+    //Array for America countries
+    let americaCountries: [String] = [
+        "Urug",
+        "Cuba",
+        "Arge",
+        "Para"
+    ]
+ 
+}
--- playground/SegmentControlTutorial/SegmentControlTutorial/ViewController.swift
+++ playground/SegmentControlTutorial/SegmentControlTutorial/ViewController.swift
@@ -13,17 +13,47 @@ class ViewController: UIViewController {
     @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var textLabel: UILabel!
     
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var thirdView: UIView!
+    @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var secondImage: UIImageView!
+    @IBOutlet weak var thirdImage: UIImageView!
+    
     @IBAction func indexChanged(sender: UISegmentedControl) {
         switch segmentControl.selectedSegmentIndex {
-        case 0:     textLabel.text = "America has selected"
-        case 1:     textLabel.text = "Europe has selected"
-        case 2:     textLabel.text = "All countries have selected"
+        case 0:
+            textLabel.text = "America has selected"
+            firstView.hidden = true
+            secondView.hidden = false
+            thirdView.hidden = false
+        case 1:
+            textLabel.text = "Europe has selected"
+            firstView.hidden = false
+            secondView.hidden = true
+            thirdView.hidden = false
+        case 2:
+            textLabel.text = "All countries have selected"
+            firstView.hidden = false
+            secondView.hidden = false
+            thirdView.hidden = true
         default:    break
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Shows emblem image -----------
+        let url: String? = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Lesser_Coat_of_Arms_of_Ukraine.svg/100px-Lesser_Coat_of_Arms_of_Ukraine.svg.png" // is waiting for the url from Resources
+        if let a = url {
+            let imgURL: NSURL? = NSURL(string: a)
+            if let url2 = imgURL {
+                let imgData: NSData? = NSData(contentsOfURL: url2)
+                if let url3 = imgData {
+                    firstImage.image = UIImage(data: url3)
+                }
+            }
+        } // ----------------------------
         // Do any additional setup after loading the view, typically from a nib.
     }
 
